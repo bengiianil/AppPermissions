685cc55b990e731266b63d797a9614e025e63d60
Bengi Anıl <bengianil34@gmail.com>
2021-09-28 14:17:20 +0300

    Initial Commit

---
+++ AppPermissions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppPermissions
+//
+//  Created by Bengi on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
